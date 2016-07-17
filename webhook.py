0e97b863a25d5f89146c81ad02234cedef14977f
Aidan Fitzgerald <duckjr159@gmail.com>
2016-07-17 09:50:35 -0400

    Threading

--- webhook.py
+++ webhook.py
@@ -1,13 +1,18 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+from socketserver import ThreadingMixIn # can use ForkingMixin instead to make a multi process system
 from urllib.parse import parse_qs
 
-class TwilioWebhook(BaseHTTPRequestHandler):
+# via https://github.com/Nakiami/MultithreadedSimpleHTTPServer
+class WebhookServer(ThreadingMixIn, HTTPServer):
+    pass
+
+class Webhook(BaseHTTPRequestHandler):
     def do_POST(self):
-        # http://stackoverflow.com/a/2124520
+        # it's in application/x-www-form-urlencoded format: via http://stackoverflow.com/a/2124520
         length = int(self.headers['Content-Length'])
         post_data = parse_qs(self.rfile.read(length).decode('utf-8'))
 
 def setup_server():
     server_addr = ('', 80)
-    httpd = HTTPServer(server_addr, TwilioWebhook)
+    httpd = WebhookServer(server_addr, Webhook)
     httpd.serve_forever()
